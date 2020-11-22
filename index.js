2e9381070a43b8b09d1a662771b3ff3e87659291
ken025 <kennethmanuel250@gmail.com>
2020-11-22 17:21:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+const random = Math.random() * 2;
+const mod = 18 % 7;
+const max = Math.max( 5, 10, 20 )
