54cf97d35cd1e37e8e7696a791ac2d2a1e78bbc6
NickRylander <nick.rylander@outlook.com>
2020-11-30 10:35:40 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 4
+const num2 = 15.5
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.random() + 1
+
+let mod = 20 % 8
+
+let max = Math.max(1, 5, 10, 15, 20)
